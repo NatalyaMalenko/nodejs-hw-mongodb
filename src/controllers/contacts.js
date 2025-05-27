a720fce3938992b25e89e13487673f988c50244c
Natalya Malenko <nataljuna@gmail.com>
2025-05-27 13:44:17 +0300

    sort, filter, pagination

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -6,9 +6,22 @@ import {
   updateContact,
 } from '../services/contacts.js';
 import createHttpError from 'http-errors';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
+import { parseFilterParams } from '../utils/parseFilterParams.js';
 
 async function getContactsController(req, res) {
-  const contacts = await allContacts();
+  const { page, perPage } = parsePaginationParams(req.query);
+  const { sortBy, sortOrder } = parseSortParams(req.query);
+  const filter = parseFilterParams(req.query);
+
+  const contacts = await allContacts({
+    page,
+    perPage,
+    sortBy,
+    sortOrder,
+    filter,
+  });
   res.status(200).json({
     status: 200,
     message: 'Successfully found contacts',
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,15 +7,32 @@ import {
   updateContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { isValidId } from '../middlewares/isValidId.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
 
 const router = express.Router();
 const jsonParser = express.json();
 
 router.get('/', ctrlWrapper(getContactsController));
 
-router.get('/:id', ctrlWrapper(getContactByIdController));
-router.post('/', jsonParser, ctrlWrapper(createContactController));
-router.patch('/:id', jsonParser, ctrlWrapper(updateContactController));
-router.delete('/:id', ctrlWrapper(deleteContactController));
+router.get('/:id', isValidId, ctrlWrapper(getContactByIdController));
+router.post(
+  '/',
+  jsonParser,
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
+router.patch(
+  '/:id',
+  isValidId,
+  jsonParser,
+  validateBody(updateContactSchema),
+  ctrlWrapper(updateContactController),
+);
+router.delete('/:id', isValidId, ctrlWrapper(deleteContactController));
 
 export default router;
