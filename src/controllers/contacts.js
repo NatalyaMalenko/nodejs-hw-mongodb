8ff019d9544dea00c4fbcba85270ee156599ad7e
Natalya Malenko <nataljuna@gmail.com>
2025-05-30 14:14:20 +0300

    fix response

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -25,7 +25,15 @@ async function getContactsController(req, res) {
   res.status(200).json({
     status: 200,
     message: 'Successfully found contacts',
-    data: contacts,
+    data: {
+      data: [contacts],
+      page: 2,
+      perPage: 4,
+      totalItems: 6,
+      totalPages: 2,
+      hasPreviousPage: true,
+      hasNextPage: false,
+    },
   });
 }
 
