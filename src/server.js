0a3de288b16a15e1ba2448452917f50ccccacf25
Natalya Malenko <nataljuna@gmail.com>
2025-06-15 21:24:39 +0300

    swagger

--- src/server.js
+++ src/server.js
@@ -8,14 +8,18 @@ import contactRouter from './routers/contacts.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { authenticate } from './middlewares/authenticate.js';
+import * as fs from 'node:fs';
 import path from 'node:path';
+import swaggerUI from 'swagger-ui-express';
 
 const PORT = Number(process.env.PORT || 3000);
+const SWAGGER_DOCUMENT = JSON.parse(
+  fs.readFileSync(path.join('docs', 'swagger.json'), 'utf-8'),
+);
 
 export const setupServer = () => {
   const app = express();
 
-  app.use('/photos', express.static(path.resolve('uploads', 'photos')));
   app.use(cookieParser());
 
   app.use(cors());
@@ -27,6 +31,10 @@ export const setupServer = () => {
       },
     }),
   );
+
+  app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(SWAGGER_DOCUMENT));
+
+  app.use('/photos', express.static(path.resolve('uploads', 'photos')));
 
   app.use('/auth', authRouter);
   app.use('/contacts', authenticate, contactRouter);
